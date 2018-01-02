46ef0d897c1fbee3d3a1794d1959f1e188a63c52
Conor McDonald <conrmcdonald@gmail.com>
2018-01-02 09:38:04 +0000

    add read data script

---
+++ read_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tf.nn.conv1d(value, filters, stride, padding)
+
+plt.plot(x)
+plt.show()
+
+pd.DataFrame(np.random.randint(1, 10, size=(3, 10), dtype=np.int))
+
+x = np.random.randn(10)
+
+np.random.choice(x, 3, replace=False)
