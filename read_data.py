1ef551cc36082b411af6f97b8bef0a344f71607d
Conor McDonald <conrmcdonald@gmail.com>
2018-01-02 17:00:50 +0000

    simple cleaning

--- read_data.py
+++ read_data.py
@@ -1,33 +1,53 @@
 import pandas as pd
 import numpy as np
 import os
+import re, string
+import begin
 
-print(os.path.dirname(os.path.realpath(__file__)))
+def init_data_setup(df, labels):
 
-train = pd.read_csv("data/train.csv")
-train = pd.DataFrame(train)
-
-targets = train.columns[2:8]
-train["no_label"] = 1-train[targets].max(axis=1)
-
-def init_data_setup(df):
-    df["no_label"] = 1-df[targets].max(axis=1)
-    df["comment_text"] = df.comment.text.str.replace("\n", " ")
+    df["no_label"] = 1-df[labels].max(axis=1)
+    df["comment_text"] = df.comment_text.str.replace("\n", " ")
+    df.comment_text.fillna("unknown", inplace=True)
 
     return df
 
 def mk_comment_stats(comment_col):
 
-    comment_col[""]
+    d = pd.DataFrame()
+
+    d["length"] = comment_col.str.len()
+    d["n_words"] = comment_col.str.split(" ").apply(lambda x: sum(1 for _ in x))
+    d["n_qm"] = comment_col.str.count("'?'")
+    d["n_exl"] = comment_col.str.count("'!")
+    return d
+
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+targets = train.columns[2:8]
+
+train = init_data_setup(train, labels=targets)
+test = init_data_setup(test, labels=targets)
 
 
+@begin.start
+def main():
 
+    folder = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(folder)
+    print("I am in {} folder".format(folder))
 
+    train = pd.read_csv("data/train.csv")
+    test = pd.read_csv("data/test.csv")
 
+    targets = train.columns[2:8]
+    print(targets)
 
-samp_text = train.comment_text.sample(100)
-samp_text
-pd.Series(samp_text).str.replace("\n", "")
+    train = init_data_setup(train, labels=targets)
+    test = init_data_setup(test)
 
+    train.to_csv("train_mod1.csv", index=False)
+    print("train saved")
 
 
